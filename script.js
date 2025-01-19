5819fb00146ae2f78793c81b4b2780b94e401dcc
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-19 11:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function firstWord(s) {
   // your code here
-}
-
+	let firstWord = "";
+	s = s.trim();
+	for(let i=0; i<s.length; i++) {
+		if(s[i] !== " ") {
+			firstWord+=s[i];
+		}else {
+			break;
+		}
+	}
+	return firstWord;
+} 
+  
 // Do not change the code below
 
 const s = prompt("Enter String:");
